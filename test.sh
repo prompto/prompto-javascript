d08001b53e2b61e7396cb3f38b6cadf9dbcefcf6
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-05 15:35:00 +0800

    use known versions of antlr4 and sync-request

--- test.sh
+++ test.sh
@@ -1,13 +1,21 @@
 # this test script assumes that 'JavaScript-Runtime' files have been copied into 'JavaScript-Code'
+echo "Testing runtime internals..."
 nodeunit JavaScript-Core/src/test/prompto/runtime/context/
-nodeunit JavaScript-Core/src/test/prompto/runtime/e/
-nodeunit JavaScript-Core/src/test/prompto/runtime/o/
 nodeunit JavaScript-Core/src/test/prompto/parser/e/
 nodeunit JavaScript-Core/src/test/prompto/parser/o/
+echo "Testing runtime with E dialect..."
+nodeunit JavaScript-Core/src/test/prompto/runtime/e/
+echo "Testing runtime with O dialect..."
+nodeunit JavaScript-Core/src/test/prompto/runtime/o/
+echo "Testing translations E -> O -> E ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/eoe/
+echo "Testing translations E -> S -> E ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/ese/
+echo "Testing translations O -> E -> O ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/oeo/
+echo "Testing translations O -> S -> O ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/oso/
+echo "Testing framework libraries ..."
 nodeunit JavaScript-Core/src/test/internet/
 nodeunit JavaScript-Core/src/test/library/e/
 nodeunit JavaScript-Core/src/test/library/reader/
