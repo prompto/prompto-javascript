b612fc5956633a57869e88ec6e597b998af72623
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-05 14:43:21 +0800

    copy runtime files into core hierarchy

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+# this test script assumes that 'JavaScript-Runtime' files have been copied into 'JavaScript-Code'
 nodeunit JavaScript-Core/src/test/prompto/runtime/context/
 nodeunit JavaScript-Core/src/test/prompto/runtime/e/
 nodeunit JavaScript-Core/src/test/prompto/runtime/o/
@@ -7,13 +8,14 @@ nodeunit JavaScript-Core/src/test/prompto/translate/eoe/
 nodeunit JavaScript-Core/src/test/prompto/translate/ese/
 nodeunit JavaScript-Core/src/test/prompto/translate/oeo/
 nodeunit JavaScript-Core/src/test/prompto/translate/oso/
-nodeunit JavaScript-Runtime/src/test/library/e/
-nodeunit JavaScript-Runtime/src/test/library/reader/
+nodeunit JavaScript-Core/src/test/internet/
+nodeunit JavaScript-Core/src/test/library/e/
+nodeunit JavaScript-Core/src/test/library/reader/
 
 # to debug a failing test:
 # place a 'debugger' instruction where you want to break
-# run the below in a shell
-# cd $ROOT_DIR/JavaScript-Core/src/test/prompto/runtime/e/
+# run something like the below in a shell
+# cd JavaScript-Core/src/test/prompto/runtime/e/
 # node --debug-brk `which nodeunit` TestNative.js (or whatever)
 # in another shell, run: node-inspector
 # point a browser to http://127.0.0.1:8080/?port=5858
