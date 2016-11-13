48190f13bb16794aab1c0724ec64aa29259a93bd
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-13 14:13:54 +0800

    fix an issue where only failures in the last test run would break the Travis build

--- test.sh
+++ test.sh
@@ -1,33 +1,48 @@
+declare -i RESULT=0
 # this test script assumes that 'JavaScript-Runtime' files have been copied into 'JavaScript-Code'
 echo "Testing runtime internals..."
 nodeunit JavaScript-Core/src/test/prompto/runtime/context/
+RESULT+=$?
 nodeunit JavaScript-Core/src/test/prompto/parser/e/
+RESULT+=$?
 nodeunit JavaScript-Core/src/test/prompto/parser/o/
+RESULT+=$?
 echo ""
 echo "Testing runtime with E dialect..."
 nodeunit JavaScript-Core/src/test/prompto/runtime/e/
+RESULT+=$?
 echo ""
 echo "Testing runtime with O dialect..."
 nodeunit JavaScript-Core/src/test/prompto/runtime/o/
+RESULT+=$?
 echo ""
 echo "Testing translations E -> O -> E ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/eoe/
+RESULT+=$?
 echo ""
 echo "Testing translations E -> S -> E ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/ese/
+RESULT+=$?
 echo ""
 echo "Testing translations O -> E -> O ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/oeo/
+RESULT+=$?
 echo ""
 echo "Testing translations O -> S -> O ..."
 nodeunit JavaScript-Core/src/test/prompto/translate/oso/
+RESULT+=$?
 echo ""
 echo "Testing native framework components ..."
 nodeunit JavaScript-Core/src/test/internet/
+RESULT+=$?
 nodeunit JavaScript-Core/src/test/reader/
+RESULT+=$?
 echo ""
 echo "Testing framework libraries ..."
 nodeunit JavaScript-Core/src/test/library/e/
+RESULT+=$?
+echo ""
+echo $RESULT
 
 # to debug a failing test:
 # place a 'debugger' instruction where you want to break
