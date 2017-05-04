e965d2a88ce7636c1f5048db778bac6ec616f2a5
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-05-05 00:23:30 +0800

    fix broken export

--- JavaScript-Runtime/src/main/prompto/io/index.js
+++ JavaScript-Runtime/src/main/prompto/io/index.js
@@ -1,2 +1,4 @@
-exports.io = require('./io');
 exports.Buffer = require('./Buffer').Buffer;
+exports.writer = require('./io').writer;
+exports.stdout = require('./io').stdout;
+exports.stderr = require('./io').stderr;
