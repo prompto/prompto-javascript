716027c1264f1674eb0e367c7eda464a7e94a904
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-09-26 23:45:26 +0800

    fix web pack issue with html encoder

--- JavaScript-Runtime/src/main/prompto/internet/index.js
+++ JavaScript-Runtime/src/main/prompto/internet/index.js
@@ -1,1 +1,5 @@
 exports.Url = require('./Url').Url;
+exports.HtmlEncoder = require('./HtmlEncoder');
+exports.htmlEncode = exports.HtmlEncoder.htmlEncode;
+exports.htmlDecode = exports.HtmlEncoder.htmlDecode;
+
