33bcaefe4619aafa307ba5b1b7025f69feeae95f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-09-26 23:46:04 +0800

    fix missing import

--- JavaScript-Core/src/main/prompto/error/index.js
+++ JavaScript-Core/src/main/prompto/error/index.js
@@ -1,3 +1,4 @@
+exports.PromptoError = require('./PromptoError').PromptoError;
 exports.SyntaxError = require('./SyntaxError').SyntaxError;
 exports.ReadWriteError = require('./ReadWriteError').ReadWriteError;
 exports.NotStorableError = require('./NotStorableError').NotStorableError;
