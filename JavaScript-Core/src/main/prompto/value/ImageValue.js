0247cac61ac7d83c591fb4237cb16dc92ffecd79
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-05-06 13:22:30 +0800

    refactor Image -> ImageValue

--- JavaScript-Core/src/main/prompto/value/ImageValue.js
+++ JavaScript-Core/src/main/prompto/value/ImageValue.js
@@ -1,12 +1,12 @@
 var BinaryValue = require("./BinaryValue").BinaryValue;
 var ImageType = require("../type/ImageType").ImageType;
 
-function Image(mimeType, data) {
+function ImageValue(mimeType, data) {
     BinaryValue.call(this, ImageType.instance, mimeType, data);
     return this;
 }
 
-Image.prototype = Object.create(BinaryValue.prototype);
-Image.prototype.constructor = Image;
+ImageValue.prototype = Object.create(BinaryValue.prototype);
+ImageValue.prototype.constructor = ImageValue;
 
-exports.Image = Image;
+exports.ImageValue = ImageValue;
