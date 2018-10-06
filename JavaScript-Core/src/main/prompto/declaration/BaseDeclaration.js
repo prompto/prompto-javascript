926d13e3396bc3ab686d9c570f1ac5c634cd3d0d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-10-06 16:23:52 +0800

    fetch original body to avoid unrecoverable errors when storing methods with errors

--- JavaScript-Core/src/main/prompto/declaration/BaseDeclaration.js
+++ JavaScript-Core/src/main/prompto/declaration/BaseDeclaration.js
@@ -26,5 +26,16 @@ BaseDeclaration.prototype.toDialect = function(writer) {
     writer.toDialect(this);
 };
 
+BaseDeclaration.prototype.fetchBody = function(parser) {
+    var section = null;
+    if(section==null && this.comments && this.comments.length > 0)
+        section = this.comments[0];
+    if(section==null && this.annotations && this.annotations.length > 0)
+        section = this.annotations[0];
+    if(section==null)
+        section = this;
+    return parser.getTokenStream().getText({ start: section.start.tokenIndex, stop: this.end.tokenIndex });
+}
+
 
 exports.BaseDeclaration = BaseDeclaration;
