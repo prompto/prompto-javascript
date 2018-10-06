63a1dd95f38bcee9779f9d261c9c6a7102dcbcb7
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-10-06 17:02:48 +0800

    fix failing tests

--- JavaScript-Core/src/main/prompto/parser/MIndentingLexer.js
+++ JavaScript-Core/src/main/prompto/parser/MIndentingLexer.js
@@ -93,7 +93,7 @@ MIndentingLexer.prototype.interpretLFTAB = function(lftab) {
 MIndentingLexer.prototype.deriveToken = function(token, type) {
 	var res = token.clone();
 	res.type = type;
-    if(token.type === ELexer.EOF)
+    if(token.type === MLexer.EOF)
         res._text = ""
 	return res;
 };
