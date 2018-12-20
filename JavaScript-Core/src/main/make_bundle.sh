26564fb1b27a2fb1564783e38c06f70effaeb106
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-12-20 07:17:04 +0000

    move js bundle from server to factory

--- JavaScript-Core/src/main/make_bundle.sh
+++ JavaScript-Core/src/main/make_bundle.sh
@@ -1,3 +1,3 @@
 webpack
-SERVER_DIR=../../../../prompto-platform/Server/src/main/resources/js/lib
-gzip -c $SERVER_DIR/prompto.core.bundle.js > $SERVER_DIR/prompto.core.bundle.js.gz
+FACTORY_DIR=../../../../prompto-factory/CodeFactory/src/main/resources/js/lib
+gzip -c $FACTORY_DIR/prompto.core.bundle.js > $FACTORY_DIR/prompto.core.bundle.js.gz
