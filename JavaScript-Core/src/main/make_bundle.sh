15cd3267f994fe8e9a272623c27e905f6b1b0113
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-03-16 18:05:34 +0800

    change build path

--- JavaScript-Core/src/main/make_bundle.sh
+++ JavaScript-Core/src/main/make_bundle.sh
@@ -1,4 +1,5 @@
-# 'npm install webpack' will install webpack cmd in node_modules
+# call 'npm install webpack' to install webpack cmd in node_modules
 ./node_modules/.bin/webpack
-FACTORY_DIR=../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib
-gzip -c $FACTORY_DIR/prompto.core.bundle.js > $FACTORY_DIR/prompto.core.bundle.js.gz
+# the below will gzip the output in prompto-platform/Server resources
+SERVER_DIR=../../../../prompto-platform/Server/src/main/resources/js/lib
+gzip -c $SERVER_DIR/prompto.core.bundle.js > $SERVER_DIR/prompto.core.bundle.js.gz
