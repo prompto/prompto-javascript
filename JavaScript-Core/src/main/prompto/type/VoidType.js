5974a6e68f6700c1a7f02bc0d38dad53a1d6b106
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-04-06 23:56:24 +0800

    fix crasher when previous assignment was illegal (thus variable type is void)

--- JavaScript-Core/src/main/prompto/type/VoidType.js
+++ JavaScript-Core/src/main/prompto/type/VoidType.js
@@ -10,7 +10,8 @@ VoidType.prototype = Object.create(NativeType.prototype);
 VoidType.prototype.constructor = VoidType;
 
 VoidType.prototype.isAssignableFrom = function(context, other) {
-    throw new Error("Should never get there!");
+    // illegal, but happens during syntax checking, if error is collected rather than thrown
+	return false;
 };
 
 VoidType.instance = new VoidType();
