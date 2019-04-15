dd21fbe4a6bc3bc9efb0f5a827127b8b1cb567b9
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-04-15 21:43:28 +0800

    copy runtime to docs

--- JavaScript-Core/src/main/make_bundle.sh
+++ JavaScript-Core/src/main/make_bundle.sh
@@ -2,3 +2,7 @@
 ./node_modules/.bin/webpack
 FACTORY_DIR=../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib
 gzip -c $FACTORY_DIR/prompto.core.bundle.js > $FACTORY_DIR/prompto.core.bundle.js.gz
+DOCS_DIR=../../../../prompto-docs/WebSite/src/web/public/js/lib
+cp -f $FACTORY_DIR/prompto.core.bundle.js $DOCS_DIR
+cp -f $FACTORY_DIR/prompto.core.bundle.js.gz $DOCS_DIR
+
