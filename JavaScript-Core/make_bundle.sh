c276b4726fc2a81d9fa7f57eced320174a91c549
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-05-11 13:32:35 +0800

    generate source map

--- JavaScript-Core/make_bundle.sh
+++ JavaScript-Core/make_bundle.sh
@@ -4,5 +4,6 @@ FACTORY_DIR=../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/
 gzip -c $FACTORY_DIR/prompto.core.bundle.js > $FACTORY_DIR/prompto.core.bundle.js.gz
 DOCS_DIR=../../prompto-docs/WebSite/src/web/public/js/lib
 cp -f $FACTORY_DIR/prompto.core.bundle.js $DOCS_DIR
+cp -f $FACTORY_DIR/prompto.core.bundle.js.map $DOCS_DIR
 cp -f $FACTORY_DIR/prompto.core.bundle.js.gz $DOCS_DIR
 
