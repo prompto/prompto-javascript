abb47fa5a89ee3252a45598f7e681ee78fa15ea8
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-07-01 21:58:39 +0800

    transpile in node

--- JavaScript-Core/src/main/prompto/jsx/JsxText.js
+++ JavaScript-Core/src/main/prompto/jsx/JsxText.js
@@ -1,5 +1,6 @@
 var IJsxExpression = require("./IJsxExpression").IJsxExpression;
 var TextType = require("../type/TextType").TextType;
+var Entities = require('html-entities').XmlEntities;
 
 function JsxText(text) {
     IJsxExpression.call(this);
@@ -27,7 +28,7 @@ JsxText.prototype.declare = function(transpiler) {
 
 JsxText.prototype.transpile = function(transpiler) {
     // convert html entities
-    var text = (new DOMParser()).parseFromString(this.text).body.textContent;
+    var text = (new Entities()).decode(this.text);
     transpiler.append(JSON.stringify(text));
 };
 
