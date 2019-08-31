5f76b755f499b84087c0fd1a830aefcf482f8572
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-08-31 15:13:43 +0800

    more tests

--- JavaScript-Core/src/test/prompto/translate/oeo/TestAnnotations.test.js
+++ JavaScript-Core/src/test/prompto/translate/oeo/TestAnnotations.test.js
@@ -8,6 +8,14 @@ test('ReactWidgetProps2', () => {
 	compareResourceOEO('annotations/ReactWidgetProps2.poc');
 });
 
+test('WidgetChildProps1', () => {
+	compareResourceOEO('annotations/WidgetChildProps1.poc');
+});
+
+test('WidgetChildProps2', () => {
+	compareResourceOEO('annotations/WidgetChildProps2.poc');
+});
+
 test('WidgetField', () => {
 	compareResourceOEO('annotations/WidgetField.poc');
 });
@@ -18,6 +26,14 @@ test('WidgetProps1', () => {
 
 test('WidgetProps2', () => {
 	compareResourceOEO('annotations/WidgetProps2.poc');
+});
+
+test('WidgetProps3', () => {
+	compareResourceOEO('annotations/WidgetProps3.poc');
+});
+
+test('WidgetProps4', () => {
+	compareResourceOEO('annotations/WidgetProps4.poc');
 });
 
 test('Callback', () => {
--- JavaScript-Core/src/test/prompto/translate/omo/TestAnnotations.test.js
+++ JavaScript-Core/src/test/prompto/translate/omo/TestAnnotations.test.js
@@ -8,6 +8,14 @@ test('ReactWidgetProps2', () => {
 	compareResourceOMO('annotations/ReactWidgetProps2.poc');
 });
 
+test('WidgetChildProps1', () => {
+	compareResourceOMO('annotations/WidgetChildProps1.poc');
+});
+
+test('WidgetChildProps2', () => {
+	compareResourceOMO('annotations/WidgetChildProps2.poc');
+});
+
 test('WidgetField', () => {
 	compareResourceOMO('annotations/WidgetField.poc');
 });
@@ -18,6 +26,14 @@ test('WidgetProps1', () => {
 
 test('WidgetProps2', () => {
 	compareResourceOMO('annotations/WidgetProps2.poc');
+});
+
+test('WidgetProps3', () => {
+	compareResourceOMO('annotations/WidgetProps3.poc');
+});
+
+test('WidgetProps4', () => {
+	compareResourceOMO('annotations/WidgetProps4.poc');
 });
 
 test('Callback', () => {
