73749ba4a323f31eaae38dbafbd954b1288fea7b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-11-03 11:22:13 +0800

    Fix typo

--- JavaScript-Core/src/main/prompto/processor/InlinedProcessor.js
+++ JavaScript-Core/src/main/prompto/processor/InlinedProcessor.js
@@ -1,7 +1,7 @@
 var AnnotationProcessor = require("./AnnotationProcessor").AnnotationProcessor;
 
 function InlinedProcessor() {
-    InlinedProcessor.call(this, "@InlinedProcessor");
+    AnnotationProcessor.call(this, "@InlinedProcessor");
     return this;
 }
 
