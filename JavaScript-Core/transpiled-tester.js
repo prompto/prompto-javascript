7f18836048e00a9108de2f9263476e310bad8c1a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-01-01 10:59:29 +0100

    refactored

--- JavaScript-Core/transpiled-tester.js
+++ JavaScript-Core/transpiled-tester.js
@@ -1,4 +1,4 @@
 var MemStoreModule = require("./src/main/prompto/memstore/MemStore");
 MemStoreModule.Cursor = Cursor;
-DataStore.instance = new MemStoreModule.MemStore();
+$DataStore.instance = new MemStoreModule.MemStore();
 main$Text_dict();
