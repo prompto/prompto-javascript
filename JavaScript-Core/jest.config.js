a51f4c35d5ce668c3214191980a32259fc196a54
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-08-05 23:16:44 +0800

    fix failing tests

--- JavaScript-Core/jest.config.js
+++ JavaScript-Core/jest.config.js
@@ -58,7 +58,7 @@ module.exports = {
   // globalTeardown: null,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: { intrinsic: {} },
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
