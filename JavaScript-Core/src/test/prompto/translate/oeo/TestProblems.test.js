5dae35bb89150d82379041929502fb2452817a64
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-30 23:39:57 +0800

    do not translate problems

--- JavaScript-Core/src/test/prompto/translate/oeo/TestProblems.test.js
+++
@@ -1,6 +0,0 @@
-var compareResourceOEO = require("../../parser/BaseParserTest").compareResourceOEO;
-
-test('Abstract', () => {
-	compareResourceOEO('problems/abstract.poc');
-});
-
--- JavaScript-Core/src/test/prompto/translate/omo/TestProblems.test.js
+++
@@ -1,6 +0,0 @@
-var compareResourceOMO = require("../../parser/BaseParserTest").compareResourceOMO;
-
-test('Abstract', () => {
-	compareResourceOMO('problems/abstract.poc');
-});
-
