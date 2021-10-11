0609957f9e4bf23db1bbc8802d65935b157a4eb4
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-10-11 17:13:50 +0200

    comments

--- JavaScript-Core/src/main/prompto/value/DbIdValue.js
+++ JavaScript-Core/src/main/prompto/value/DbIdValue.js
@@ -25,6 +25,7 @@ export default class DbIdValue extends Value {
     }
 
     toJson(context, json, instanceId, fieldName, withType, binaries) {
+        // need a type because value could be a non native JSON type such as UUID
         const value = withType ? { type: DbIdType.instance.name, value: this.value } : this.value;
         if(Array.isArray(json))
             json.push(value);
