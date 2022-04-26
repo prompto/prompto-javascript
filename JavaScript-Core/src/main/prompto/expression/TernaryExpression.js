5a54d3f2f2b10231747c3ff76849716e8ef8ede7
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-04-27 00:32:35 +0200

    support downcasting in ternary expressions

--- JavaScript-Core/src/main/prompto/expression/TernaryExpression.js
+++ JavaScript-Core/src/main/prompto/expression/TernaryExpression.js
@@ -2,6 +2,7 @@ import Expression from './Expression.js'
 import { Dialect } from '../parser/index.js'
 import { BooleanType, TypeMap } from '../type/index.js'
 import { BooleanValue } from '../value/index.js'
+import { EqualsExpression } from "./index.js";
 
 export default class TernaryExpression extends Expression {
 
@@ -13,7 +14,7 @@ export default class TernaryExpression extends Expression {
     }
 
     toDialect(writer) {
-        if(writer.dialect==Dialect.O) {
+        if(writer.dialect === Dialect.O) {
             this.condition.toDialect(writer);
             writer.append(" ? ");
             this.ifTrue.toDialect(writer);
@@ -32,17 +33,19 @@ export default class TernaryExpression extends Expression {
         const type = this.condition.check(context);
         if(!(type instanceof BooleanType))
             context.problemListener.reportIllegalAssignment(this.condition, BooleanType.instance, type);
-        const trueType = this.ifTrue.check(context);
-        const falseType = this.ifFalse.check(context);
+        if(this.condition instanceof EqualsExpression)
+            context = this.condition.downcast(context, false);
         const types = new TypeMap();
-        types.add(trueType);
-        types.add(falseType);
+        types.add(this.ifTrue.check(context));
+        types.add(this.ifFalse.check(context));
         return types.inferType(context, this);
     }
 
     interpret(context) {
         const test = this.condition.interpret(context);
-        if(test == BooleanValue.TRUE)
+        if(this.condition instanceof EqualsExpression)
+            context = this.condition.downcast(context, true);
+        if(test === BooleanValue.TRUE)
             return this.ifTrue.interpret(context);
         else
             return this.ifFalse.interpret(context);
@@ -50,6 +53,10 @@ export default class TernaryExpression extends Expression {
 
     declare(transpiler) {
         this.condition.declare(transpiler);
+        if(this.condition instanceof EqualsExpression) {
+            var context = this.condition.downcast(transpiler.context, false);
+            transpiler = transpiler.newChildTranspiler(context);
+        }
         this.ifTrue.declare(transpiler);
         this.ifFalse.declare(transpiler);
     }
@@ -57,10 +64,15 @@ export default class TernaryExpression extends Expression {
     transpile(transpiler) {
         transpiler.append("(");
         this.condition.transpile(transpiler);
+        if(this.condition instanceof EqualsExpression) {
+            var context = this.condition.downcast(transpiler.context, false);
+            transpiler = transpiler.newChildTranspiler(context);
+        }
         transpiler.append(" ? ");
         this.ifTrue.transpile(transpiler);
         transpiler.append(" : ");
         this.ifFalse.transpile(transpiler);
         transpiler.append(")");
+        transpiler.flush();
     }
 }
