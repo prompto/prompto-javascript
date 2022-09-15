c9408022aa66a1df43519a14d393107c8f20424a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-09-15 14:13:22 +0200

    drop Fiber usage since it is not available with node 16

--- JavaScript-Runtime/src/main/prompto/utils/Utils.js
+++ JavaScript-Runtime/src/main/prompto/utils/Utils.js
@@ -1,41 +1,17 @@
-var Fiber = null;
-// #if process.env.NODE_ENV === 'DEVELOPMENT'
-Fiber = require("fibers"); // nodejs only, see Core/webpack.config.js
-// #endif
-
-function doSleepFiber(millis) {
-    var fiber = Fiber.current;
-    setTimeout(function() {
-        fiber.run();
-    }, millis);
-    return Fiber.yield();
-}
-
 /* global Int32Array */
-function doSleepAtomic(millis) {
-    while (millis > 0) {
-        Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 10);
-        millis -= 10;
-    }
-}
-
 function doSleep(millis) {
     try {
-        if (Fiber && Fiber.current)
-            doSleepFiber(millis);
-        else
-            doSleepAtomic(millis);
+        while (millis > 0) {
+            Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 10);
+            millis -= 10;
+        }
     } catch(e) {
         console.log(e.stack);
     }
 }
 
 function sleep(millis) {
-    // var before = new Date();
     doSleep(millis);
-    // var after = new Date();
-    // var elapsed = after.valueOf() - before.valueOf();
-    // console.log("elapsed: " + elapsed);
 }
 
 exports.sleep = sleep;
